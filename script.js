50bfeac6101321020d56095bb02d10a5de51faea
BLinks12 <honorforgeai@gmail.com>
2024-11-30 07:36:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling
+function scrollToSection(sectionId) {
+  document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+
+// Play sound effect
+function playCandySound() {
+  document.getElementById('candy-sound').play();
+}
